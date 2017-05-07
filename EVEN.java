46c0d279a27e5ea6b9de1512d81c4788348ab11c
githubuser30 <artish2008@gmail.com>
2017-05-07 08:37:37 -0700

    Add files via upload

---
+++ EVEN.java
@@ -0,0 +1,31 @@
+package com.main.JavaPractice;
+
+public class EVEN {
+
+	
+
+			public static void findOddEven() {
+
+				int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+				for (int i = 0; i < numbers.length; i++) {
+
+					if (numbers[i] % 2 == 0) {
+
+						System.out.println(numbers[i] + " is even");
+					} else
+						System.out.println(numbers[i] + " is odd");
+
+				}
+			}
+
+			public static void main(String[] args) {
+
+				EVEN.findOddEven();
+			}
+
+		
+
+	}
+
+
